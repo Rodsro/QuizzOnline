121f9a9d8e067128e1c34956a2051a2c2acdce88
Rodrigo Oliveira <119442868+Rodsro@users.noreply.github.com>
2023-04-25 16:20:39 -0300

    First Commit

---
+++ questions.js
@@ -0,0 +1,95 @@
+let questions = [
+    {
+        question: 'Qual é a capital da Australia?',
+        options: [
+            'Sydney',
+            'Camberra',
+            'Melbourne',
+            'Brasilia'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Se uma pessoa utiliza os membros esquerdos e direito com a mesma destreza, como se chama?',
+        options: [
+            'Destro e Canhoto',
+            'Chuta com as duas',
+            'Ambidestro'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Quem pesa mais: 1kg de algodão ou 1kg de titânio?',
+        options: [
+            'Algodão',
+            'Possuem o mesmo peso',
+            'Titânio'
+        ],
+        answer: 1
+    },
+    {
+        question: 'O valor de 50 é 50% da metade de 200?',
+        options: [
+            'Falso',
+            'Verdadeiro'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual a raiz quadrada de 144?',
+        options: [
+            '9',
+            '10',
+            '11',
+            '12'
+        ],
+        answer: 3
+    },
+    {
+        question: 'Quantos times disputam o campeonato brasileiro da serie A?',
+        options: [
+            '16',
+            '18',
+            '20'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Quantas casas tem um tabuleiro de xadrez?',
+        options: [
+            '50',
+            '58',
+            '64'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Quantas são as cores primárias?',
+        options: [
+            '1',
+            '2',
+            '3',
+            '4'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Qual a capital de Sergipe?',
+        options: [
+            'Aracaju',
+            'Maceio',
+            'João Pessoa',
+            'Sergipe'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Quanto é 5 + 3!?',
+        options: [
+            '11',
+            '8',
+            '1'
+        ],
+        answer: 0
+    },
+];
